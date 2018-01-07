4c449ef60250999b0055fe5dccdc7cd54113a18a
Jérome <jerome.morino@verallia.com>
2018-01-07 22:50:01 +0100

    improve execution flow and options

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
+#!/usr/bin/env node
+
 import program from 'commander'
 
-program
-	.version('0.1.0')
-	.option('-p, --peppers', 'Add peppers')
-	.option('-P, --pineapple', 'Add pineapple')
-	.option('-b, --bbq-sauce', 'Add bbq sauce')
-	.option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-	.parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+program
+	.version('0.1.1')
+	.description('System backup manager')
+	.option('-d, --dry-run', 'simulate the command but do not write any file')
+	.option('-c, --config <path>', 'set config path. defaults to /root/.backup/config.json');
+
+program
+	.command('backup [type]')
+	.description('perform a backup of the system')
+	.action((type, options) => {
+		console.log(type, 'backup', options.config, options.dryRun);
+	});
+
+program
+	.command('restore [date]')
+	.description('restore the system')
+	.action((date, options) => {
+		console.log('restore @', date, options.config, options.dryRun);
+	});
+
+program
+	.command('list')
+	.description('list all availbale backups')
+	.action((options) => {
+		console.log('run list', options.config, options.dryRun);
+	});
+
+
+program.parse(process.argv);
+
+// default: no command
+if (!process.argv.slice(2).length) {
+	program.help();
+}
