5745ae31ac0f045882acbe4cf68932f1710cab63
Jérome <jerome.morino@verallia.com>
2018-01-11 01:40:38 +0100

    add restorer

---
+++ src/BackupRestorer.js
@@ -0,0 +1,90 @@
+import { mkdirSync } from 'fs'
+import path, { relative } from 'path'
+import { execSync } from 'child_process'
+import chalk from 'chalk'
+import List from './List'
+import { BACKUP_EXT } from './defaults'
+import { BackupNotFoundError } from './errors'
+
+
+export default class BackupRestorer {
+
+	constructor({ config, options }) {
+		this.config = config;
+		this.options = options;
+	}
+
+	//=================================================================================================================
+
+	restore(date) {
+		const isIncr = !!this.options.incr;
+		const isDryRun = !!this.options.dryRun;
+		const filename = `${date}.${BACKUP_EXT}`;
+
+		// find the parent backup to restore
+		const list = new List(this.config);
+		const backups = list.findRestorationChain(date);
+		if (!backups.length) { throw new BackupNotFoundError(`Backup ${date} not found`); }
+
+		console.log('');
+		console.log('Restoring backups:');
+		console.log('------------------');
+		backups.forEach(backup => {
+			console.log(chalk.red('  ' + backup.name));
+			this._exec(backup);
+		});
+		console.log('');
+
+		// 	// TODO check if parent === basename: backup already exists for today
+
+		// 	// run backup
+		// 	const dir = path.parse(parent.filepath).dir;
+		// 	this._exec({ dir, filename });
+		// }
+		// else {
+		// 	// create a new dir for the full backup
+		// 	const dir = path.resolve(path.join(this.config.backupDir, basename));
+		// 	if (this.options.dryRun) {
+		// 		console.log(chalk.bold.red('[DRY-RUN]'), chalk.red('create directory:'), dir);
+		// 	}
+		// 	else {
+		// 		mkdirSync(dir);
+		// 	}
+
+		// 	// run backup
+		// 	this._exec({ dir, filename });
+		// }
+	}
+
+
+	//=============================================================================
+	//=========================== Private Methods =================================
+	//=============================================================================
+
+
+	_exec({ filepath }) {
+		const target = path.resolve(this.config.target);
+		
+		// build command line: tar -xvpzf "$dir/$f" --numeric-owner >> $logFile 2>> $errFile
+		const cmd = ['tar'];
+		cmd.push('--extract');
+		cmd.push('--verbose');
+		cmd.push('--preserve-permissions');
+		cmd.push('--gzip');
+		cmd.push('--file', `"${filepath}"`);
+		cmd.push('--listed-incremental=/dev/null'); // useful ??
+		cmd.push('--numeric-owner'); // preserve extracted files' UID
+
+		// build final command line
+		const cmdLine = cmd.join(' ');
+		
+		// execute command
+		if (this.options.dryRun) {
+			console.log(chalk.bold.red('[DRY-RUN]'), chalk.red("cd'ing to:"), target);
+			console.log(chalk.bold.red('[DRY-RUN]'), chalk.red('execute command:'), cmdLine);
+		}
+		else {
+			const result = execSync(cmdLine, { cwd : target });
+		}
+	}
+}
--- src/errors.js
+++ src/errors.js
@@ -1,3 +1,5 @@
 export class FileNotFoundError         extends Error { get ERRNO() { return 1 }}
 export class InvalidJSONFileError      extends Error { get ERRNO() { return 2 }}
 export class InvalidConfigurationError extends Error { get ERRNO() { return 3 }}
+
+export class BackupNotFoundError       extends Error { get ERRNO() { return 4 }}
